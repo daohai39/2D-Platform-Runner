254c27dc25535f6886367f86c8f794378a7729c6
Hai Dao <daohai39@gmail.com>
2018-05-10 11:21:52 +0700

    Add animation for Boss

--- Assets/Scripts/Components/MovementConstraint.cs
+++ Assets/Scripts/Components/MovementConstraint.cs
@@ -4,16 +4,7 @@ using UnityEngine;
 
 public class MovementConstraint : MonoBehaviour {
 	[SerializeField] private Boundary boundary;
-	private Player player;
-
-	/// <summary>
-	/// Start is called on the frame when a script is enabled just before
-	/// any of the Update methods is called the first time.
-	/// </summary>
-	void Start()
-	{
-		player = GetComponent<Player>();	
-	}
+	[SerializeField] private GameObject player;
 	/// <summary>
 	/// Update is called every frame, if the MonoBehaviour is enabled.
 	/// </summary>
